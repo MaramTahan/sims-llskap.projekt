6dd7023b5f9d9c432001e19963ceac65d9e4c8cd
Maram Tahan <111732496+MaramTahan@users.noreply.github.com>
2022-10-04 21:41:03 +0200

    change

--- script.js
+++ script.js
@@ -1,25 +1,26 @@
+
 function myFunction() {
  let name = document.getElementById("name").value;
  let psw = document.getElementById("psw").value;
  
- //here save user input in localStorage
- localStorage.setItem('name',(name));
- localStorage.setItem('psw',(psw));
+ localStorage.setItem("name","Sara");
+ localStorage.setItem("psw","qwe123");
  
- /*here use if-sats so if user login with correct name and password,
- get greeted message*/
+ 
  if (name === "Sara" && psw === "qwe123") {
   document.getElementById("hello").innerHTML = "<p>Välkommen, du är nu inloggad</p>";
-  
- }/*otherwise appear a message to login again*/ 
+  document.getElementById("button").style.visibility = "hidden";
+ }
  else {
   document.getElementById("hello").innerHTML = "<p>Lösenordet eller användarnamnet är felaktigt, gärna försök igen</p>"
-  location.reload();//empty user input
+  let formTag = document.querySelector("form");
+  let btnAgain = document.createElement("button");
+  let btnAgainText = document.createTextNode("Försöka Igen");
+  btnAgain.appendChild(btnAgainText);
+  formTag.appendChild(btnAgain);
  }
 }
 
-
-//logout when user click in logout button
 logoutUrl = "./index.html"
 function idLogout() {
  window.location = logoutUrl;
