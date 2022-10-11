38165a95daea7bb677e90b7a16a280186fdebef0
Maram Tahan <111732496+MaramTahan@users.noreply.github.com>
2022-10-11 21:30:57 +0200

    hidden logout button

--- script.js
+++ script.js
@@ -1,16 +1,22 @@
-
+//hidden logout button
+document.getElementById("logoutbtn").style.visibility = "hidden";
+ 
 function myFunction() {
  let name = document.getElementById("name").value;
  let psw = document.getElementById("psw").value;
- 
+ //here save user input in localStorage
  localStorage.setItem("name","Sara");
  localStorage.setItem("psw","qwe123");
- 
+ /*here use if-sats so if user login with correct name and password,
+ get greeted message*/
  if (name === "Sara" && psw === "qwe123") {
   document.getElementById("hello").innerHTML = "<p>Välkommen, du är nu inloggad</p>";
-  document.getElementById("button").style.visibility = "hidden";
+  //visibility buttons
+  document.getElementById("loginbtn").style.visibility = "hidden";
+  document.getElementById("logoutbtn").style.visibility = "visible";
  }
  else {
+  /*otherwise appear a message to login again*/
   document.getElementById("hello").innerHTML = "<p>Lösenordet eller användarnamnet är felaktigt, gärna försök igen</p> <br>"
   let formTag = document.querySelector("form");
   let btnAgain = document.createElement("button");
@@ -19,7 +25,7 @@ function myFunction() {
   formTag.appendChild(btnAgain);
  }
 }
-
+//logout when user click in logout button
 logoutUrl = "./index.html"
 function idLogout() {
  window.location = logoutUrl;
